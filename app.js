b1a6775664bfdd9944771e9a95191bd1f73762e1
philawsophizing <philawsophizing@gmail.com>
2017-07-18 20:29:54 -0600

    Init server, serve up static route

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+// SERVER
+const app = express();
+const port = 1500;
+
+// ROUTES
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Podd running on ${port}`);
+});
