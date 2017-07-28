f8b968e71f00c2a015c7dd2c6a6dd8c0179d4045
philawsophizing <philawsophizing@gmail.com>
2017-07-28 01:14:00 -0600

    Include new files to be bundled

--- bundle.js
+++ bundle.js
@@ -25,21 +25,28 @@ const fileWatch = () => {
 const buildFiles = {
   js: [
     './dev/js/index.js',
-    './dev/js/header.js'
+    './dev/js/update_view.js',
+    './dev/js/customer_home.js',
+    './dev/js/merchant_home.js',
+    './dev/js/DB.js',
+    './dev/js/stores.js',
+    './dev/js/create_store.js'
   ],
   css: [
     './dev/css/reset.css',
     './dev/css/app_shell.css',
-    './dev/css/header.css',
     './dev/css/customer_home.css',
-    './dev/css/merchant_home.css'
+    './dev/css/merchant_home.css',
+    './dev/css/stores.css',
+    './dev/css/create_store.css'
   ],
   html: [
     './dev/html/open.html',
     // PAGES ------------->
-    './dev/html/header.html',
     './dev/html/customer_home.html',
     './dev/html/merchant_home.html',
+    './dev/html/stores.html',
+    './dev/html/create_store.html',
     // <-------------------
     './dev/html/close.html'
   ]
