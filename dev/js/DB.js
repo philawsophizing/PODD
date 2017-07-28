69fb055e2dc11011e7e27a96cd42b192173e23b1
philawsophizing <philawsophizing@gmail.com>
2017-07-28 01:11:45 -0600

    Init simple DB

---
+++ dev/js/DB.js
@@ -0,0 +1,25 @@
+const DB = {
+  stores: [
+    {
+      type: 'Retail',
+      name: 'Lululemon',
+      address: '17 ave SW, Calgary',
+      lat: 88888888,
+      lon: 3837484,
+    },
+    {
+      type: 'Restaurant',
+      name: 'BlueStar',
+      address: '1 ave NE, Calgary',
+      lat: 88888888,
+      lon: 3837484,
+    },
+    {
+      type: 'Retail',
+      name: 'Dollarama',
+      address: '8 ave SW, Calgary',
+      lat: 88888888,
+      lon: 3837484,
+    },
+  ]
+};
